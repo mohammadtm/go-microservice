72fd8e295fc4df66fbe1f5d6b00699b459ac545a
MohammadHosein Abbasi <mohammadhoseinabbasist@yahoo.com>
2024-09-24 10:16:50 +0330

    middleware added

--- main.go
+++ main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
 	server := &http.Server{
-		Addr: ":8080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("Hello World"))
-		}),
+		Addr:    ":8080",
+		Handler: router,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
 }
